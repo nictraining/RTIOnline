0bf75d2e41450bf0f4428f1c04cd63f4d8cd92b8
Saumya0007 <saumyadipdeb18@gmail.com>
2025-07-15 17:21:47 +0530

    Update LoginResponseNodalDTO.java
    NodalLoginResponseDTO-by Saumyadip Deb

--- rti-master/src/main/java/nic/rti/master/dto/LoginResponseNodalDTO.java
+++ rti-master/src/main/java/nic/rti/master/dto/LoginResponseNodalDTO.java
@@ -5,9 +5,18 @@ public class LoginRequestDto {
     private String password;
 
     // Getters and Setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() {
+        return username;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-}
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
